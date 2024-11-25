6984b07ca5166e3a1c83e9c28596506779b3459c
hugo salazar <hugog.salazarl@gmail.com>
2024-11-25 17:09:08 -0500

    update 1 file and create 1 file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import  Navbar  from "@/components/ui/navbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -35,7 +36,10 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <main className="container mx-auto">
+          <Navbar />
           {children}
+          </main>
         </ThemeProvider>
       </body>
     </html>
