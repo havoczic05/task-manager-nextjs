b87b096cd43b416f93286acd95e1b2c9c7103cbf
hugo salazar <hugog.salazarl@gmail.com>
2024-11-29 11:32:06 -0500

    fix: new/page error - Tasform not passing correct "task" properties

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -3,7 +3,7 @@ import {TaskForm} from "./task-form"
 function NewPage() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <TaskForm />
+      <TaskForm task={{ name: '', id: 0, description: null, priority: '', createdAt: new Date(), updatedAt: new Date() }} />
     </div>
   )
 }
