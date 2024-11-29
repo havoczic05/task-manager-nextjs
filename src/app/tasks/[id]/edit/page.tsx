91fce4829366957a91998c2d6b3825be5df6949c
hugo salazar <hugog.salazarl@gmail.com>
2024-11-29 10:50:13 -0500

    await params for nextjs 15

--- src/app/tasks/[id]/edit/page.tsx
+++ src/app/tasks/[id]/edit/page.tsx
@@ -2,18 +2,15 @@ import { TaskForm } from "@/app/new/task-form";
 import prisma from "@/lib/prisma";
 import { redirect } from "next/navigation";
 
-
+type Params = Promise<{ id: string[] }>;
 export default async function TaskPageEdit({ params }: {
-  params: {
-    id: string
-  }
-}) {
+  params: Params}) {
 
 
-
+    const { id } = await params;
   const task = await prisma.task.findFirst({
     where: {
-      id: parseInt(params.id)
+      id: parseInt(id[0])
     }
   })
 
