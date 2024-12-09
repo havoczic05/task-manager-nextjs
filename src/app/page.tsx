3d5f8afeb3bc03b3806d3e96a8a66f3e6fd9bef6
hugo salazar <hugog.salazarl@gmail.com>
2024-12-09 00:43:40 -0500

    fix: reversing tasks before being mapped in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default async function HomePage() {
 
   return (
     <div className="grid  sm:grid-cols-1 mx-6 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {tasks.map((task) => (
+      {tasks.reverse().map((task) => (
         <TaskCard key={task.id} task={task} />
 
       ))}
